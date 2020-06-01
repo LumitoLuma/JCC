a00e97ce2e4446b4b6a1371ceb692c7d8974bcdd
Lumito <50250351+LumitoLuma@users.noreply.github.com>
2020-06-01 23:27:17 +0200

    Added help.c file for program help.

---
+++ utils/help.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("      _   ___  ___ \n");
+    printf("     | | |  _||  _|\n");
+    printf("  _  | | | |  | |  \n");
+    printf(" | |_| | | |_ | |  \n");
+    printf(" |_____| |___||___|\n\n");
+    printf("JCC Command Line Interface.\n\n");
+    printf("Usage: \'JCC.bat [maven | gradle | ant | help]\'\n\n");
+    printf("Options:\n");
+    printf(" maven  - Runs the maven compiler\n");
+    printf(" gradle - Runs the gradle compiler\n");
+    printf(" ant    - Runs the ant compiler\n");
+    printf(" help   - Displays this screen\n\n---------------------------------------------------------------\n");
+    printf("Remember that the compiler arguments go after the compiler name\n---------------------------------------------------------------\n");
+}
