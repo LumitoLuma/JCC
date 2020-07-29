669bcb83179b14e27f3dc896dfde5f1d1746f345
Lumito <50250351+LumitoLuma@users.noreply.github.com>
2020-07-30 00:50:04 +0200

    Added program version function in install.bat

--- install.bat
+++ install.bat
@@ -1,6 +1,12 @@
 @echo off
 endlocal
 chcp 65001 >NUL
+IF /I "%~1"=="version"    GOTO version
+IF /I "%~1"=="/version"   GOTO version
+IF /I "%~1"=="-version"   GOTO version
+IF /I "%~1"=="v"          GOTO version
+IF /I "%~1"=="/v"         GOTO version
+IF /I "%~1"=="-v"         GOTO version
 echo ===================================================
 echo  Java Compiler Colection (JCC) installation script
 echo ===================================================
@@ -128,3 +134,31 @@ if ERRORLEVEL == 1 (echo. && echo Installation error. Process aborted. && popd &
 echo --- Installation finished! Enjoy JCC!
 timeout /nobreak 1 >NUL 2>NUL
 exit /b 0
+
+:version
+echo Java Compiler Collection version 1.0
+echo Coded by Lumito - www.lumito.net
+echo Hosted on GitHub: github.com/LumitoLuma
+echo.
+echo Licensed under the MIT License
+echo.
+echo Copyright (c) 2020 Lumito Luma
+echo.
+echo Permission is hereby granted, free of charge, to any person obtaining a copy
+echo of this software and associated documentation files (the "Software"), to deal
+echo in the Software without restriction, including without limitation the rights
+echo to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+echo copies of the Software, and to permit persons to whom the Software is
+echo furnished to do so, subject to the following conditions:
+echo.
+echo The above copyright notice and this permission notice shall be included in all
+echo copies or substantial portions of the Software.
+echo.
+echo THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+echo IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+echo FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+echo AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+echo LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+echo OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+echo SOFTWARE.
+exit /b
